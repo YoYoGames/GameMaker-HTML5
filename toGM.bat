a304c41a106d0baef7c33187e0e35a6eac04bddd
Daniel Collins <daniel.collins@yoyogames.com>
2022-09-12 10:58:27 +0100

    Remove obsolete cruft

--- toGM.bat
+++
@@ -1,4 +0,0 @@
-copy .\Windows8\js\*.* "c:\source\GameMaker\Game Maker 8.0\5pice\Windows8\Windows8\js" /y
-copy .\Windows8\zip-js\*.* "c:\source\GameMaker\Game Maker 8.0\5pice\Windows8\Windows8\zip-js" /y
-copy .\Windows8\css\*.* "c:\source\GameMaker\Game Maker 8.0\5pice\Windows8\Windows8\css" /y
-copy .\Windows8\data\*.* "c:\source\GameMaker\Game Maker 8.0\5pice\Windows8\Windows8\data" /y
--- web.bat
+++
@@ -1,2 +0,0 @@
-"C:\source\GameMaker\Runner\HTML5\bin\tiny.exe" "C:\source\GameMaker\Runner\HTML5" 80 192.168.11.60
-
