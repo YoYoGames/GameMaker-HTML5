b6d646d89215547a91b0823d0aae17cbae21eae4
Toby Galal <tgalal@opera.com>
2022-10-10 15:30:25 +0100

    Remove unrecognised characters and avoid returning object literals

--- scripts/yyTime.js
+++ scripts/yyTime.js
@@ -10,7 +10,7 @@
 //
 // **********************************************************************************************************************
 
-﻿/* Built-in time source IDs */
+/* Built-in time source IDs */
 var eTimeSource_Global = 0,
     eTimeSource_Game = 1,
     eTimeSource_SDParent = 2;
