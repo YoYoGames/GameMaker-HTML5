637af22f404087d9b0a8bec6b292c2840202b144
Toby Galal <tgalal@opera.com>
2022-10-17 15:01:23 +0100

    GM-5586 Add GML fn audio_effect_create, effect structs and internal bus routing

--- scripts/sound/AudioEffect.js
+++ scripts/sound/AudioEffect.js
@@ -1,5 +1,7 @@
-class AudioEffect
+class AudioEffect extends AudioWorkletNode
 {
+    static PARAM_TIME_CONSTANT = 0.005; // 5ms
+
     static Type = {
         Bitcrusher: 0,
 		Delay: 1,
@@ -9,8 +11,91 @@ class AudioEffect
 		Reverb1: 5
     };
 
-    process(input)
+    static Create(_type)
     {
-        return;
+        switch (_type)
+        {
+            case AudioEffect.Type.Bitcrusher:   return new BitcrusherEffect();
+            case AudioEffect.Type.Delay:        return new DelayEffect();
+            case AudioEffect.Type.Gain:         return new GainEffect();
+            case AudioEffect.Type.HPF2:         return new HPF2Effect();
+            case AudioEffect.Type.LPF2:         return new LPF2Effect();
+            case AudioEffect.Type.Reverb1:      return new Reverb1Effect();
+            default:                            return null;
+        }
+    }
+
+    constructor(_workletName)
+    {
+        super(g_WebAudioContext, _workletName, { 
+			numberOfInputs: 1,
+			numberOfOutputs: 1, 
+			outputChannelCount: [2]
+		});
+    }
+}
+
+class AudioEffectStruct
+{
+    static Create(_type)
+    {
+        switch (_type)
+        {
+            case AudioEffect.Type.Bitcrusher:   return new BitcrusherEffectStruct();
+            case AudioEffect.Type.Delay:        return new DelayEffectStruct();
+            case AudioEffect.Type.Gain:         return new GainEffectStruct();
+            case AudioEffect.Type.HPF2:         return new HPF2EffectStruct();
+            case AudioEffect.Type.LPF2:         return new LPF2EffectStruct();
+            case AudioEffect.Type.Reverb1:      return new Reverb1EffectStruct();
+            default:                            return null;
+        }
+    }
+
+    constructor(_type)
+    {
+        // GML object props
+		this.__type = "[AudioEffect]";
+		this.__yyIsGMLObject = true;
+
+        this.nodes = [];
+
+        this.type = _type;
+        this.params = {
+            bypass: false
+        };
+        
+        // Define user-facing properties
+		Object.defineProperties(this, {
+			gmlbypass: {
+				enumerable: true,
+				get: () => {
+                    return this.params.bypass;  
+                },
+				set: (_state) => {
+                    this.params.bypass = yyGetBool(_state);
+
+                    this.nodes.foreach((_node) => {
+                        const bypass = _node.parameters.get("bypass");
+                        bypass.value = this.params.bypass;
+                    });
+                }
+			}
+		});
+    }
+
+    addNode()
+    {
+        const node = AudioEffect.Create(this.type);
+        this.nodes.push(node);
+        
+        return node;
+    }
+
+    removeNode(_node) 
+    {
+        const idx = this.nodes.findIndex(_node);
+
+        if (idx != -1)
+            this.nodes.splice(idx, 1);
     }
 }
---
+++ scripts/sound/effects/Delay.js
@@ -0,0 +1,65 @@
+class DelayEffect extends AudioEffect
+{
+    constructor()
+    {
+        super("delay-processor");
+    }
+}
+
+class DelayEffectStruct extends AudioEffectStruct
+{
+    constructor()
+    {
+        super(AudioEffect.Type.Delay);
+
+        this.params.time = 0.0;
+        this.params.feedback = 0.0;
+        this.params.mix = 0.0;
+
+        // Define user-facing properties
+		Object.defineProperties(this, {
+			gmltime: {
+				enumerable: true,
+				get: () => {
+                    return this.params.time;
+                },
+				set: (_time) => {
+                    this.params.time = clamp(_time, 0.0, 5.0);
+
+                    this.nodes.forEach((_node) => {
+                        const time = _node.parameters.get("time");
+                        time.setTargetAtTime(this.params.time, 0, AudioEffect.PARAM_TIME_CONSTANT);
+                    });
+                }
+			},
+            gmlfeedback: {
+				enumerable: true,
+				get: () => {
+                    return this.params.feedback;
+                },
+				set: (_feedback) => {
+                    this.params.feedback = clamp(_feedback, 0.0, 1.0);
+
+                    this.nodes.forEach((_node) => {
+                        const feedback = _node.parameters.get("feedback");
+                        feedback.setTargetAtTime(this.params.feedback, 0, AudioEffect.PARAM_TIME_CONSTANT);
+                    });
+                }
+			},
+            gmlmix: {
+				enumerable: true,
+				get: () => {
+                    return this.params.mix;
+                },
+				set: (_mix) => {
+                    this.params.mix = clamp(_mix, 0.0, 1.0);
+
+                    this.nodes.forEach((_node) => {
+                        const mix = _node.parameters.get("mix");
+                        mix.setTargetAtTime(this.params.mix, 0, AudioEffect.PARAM_TIME_CONSTANT);
+                    });
+                }
+			}
+		});
+    }
+}
