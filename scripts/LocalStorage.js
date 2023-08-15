87622070afe42ede327a7eab3b90e7e9b72fb720
YellowAfterlife <yellowafterlife@hotmail.com>
2023-08-15 11:50:27 +0300

    We can use a regex instead of a whole parser for GetLocalStorageRoot.

--- scripts/LocalStorage.js
+++ scripts/LocalStorage.js
@@ -35,25 +35,8 @@ function SupportsLocalStorage()
 function GetLocalStorageRoot()
 {
     // Only use alpha-numeric characters, and underbars, from the display name
-    var displayName = "";
-    if (g_pGMFile.Options.DisplayName !== null && g_pGMFile.Options.DisplayName !== undefined) {
-    
-        for (var i = 0; i < g_pGMFile.Options.DisplayName.length; i++) {
-        
-            var chr = g_pGMFile.Options.DisplayName[i];
-            if ((chr >= 'a' && chr <= 'z') ||
-                (chr >= 'A' && chr <= 'Z') ||
-                (chr >= '0' && chr <= '9') ||
-                (chr === '_'))
-            {
-                displayName = displayName + g_pGMFile.Options.DisplayName[i];
-            }
-        }
-    }
-    if (displayName.length === 0) {
-        displayName = "GameMaker";
-    }
-    return displayName + "." + g_pBuiltIn.game_id + ".";
+    let _displayName = (g_pGMFile.Options.DisplayName || "").replace(new RegExp("[^\\w]", "g"), "");
+    return (_displayName || "GameMaker") + "." + g_pBuiltIn.game_id + ".";
 }
 
 // #############################################################################################
