f0d3ba79bbc8ed21620c40b71d6561861348ddb2
Fritz <1914610+jackerley@users.noreply.github.com>
2023-09-19 11:08:11 +0100

    [HTML5] Fixes for return values of instance_position and position_meeting
    https://github.com/YoYoGames/GameMaker-Bugs/issues/1682

--- scripts/functions/Function_Movement.js
+++ scripts/functions/Function_Movement.js
@@ -967,8 +967,8 @@ function position_meeting(_pInst,_x,_y,_obj)
 		else
 		{
 			var id = Command_CollisionPoint(_pInst,_x,_y,_obj,true,false);
-			
-			return id;
+			if(id != OBJECT_NOONE)
+				return true;
 		}
 	}
 	else if (_obj instanceof Array)
@@ -987,18 +987,19 @@ function position_meeting(_pInst,_x,_y,_obj)
 			else
 			{
 				var id = Command_CollisionPoint(_pInst,_x,_y,obj2,true,false);
-				if(id==true)
+				if(id!=OBJECT_NOONE)
 					return true;
 			}
 		}
-		return false;
+		
 	}
 	else
 	{
 		var id = Command_CollisionPoint(_pInst,_x,_y,_obj,true,false);
-		
-		return id;
+		if(id!=OBJECT_NOONE)
+			return true;
 	}
+	return false;
 }
 
 
