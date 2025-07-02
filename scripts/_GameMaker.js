6c77e72b8246943371b96e03729b28e6afaaf292
Mike Rennie <mrennie@opera.com>
2025-07-02 11:21:09 +0100

    Tweaked test a bit so it works even if LegacyPrimitiveDrawing is undefined
    This means that the fix doesn't rely on having to have a specific version of the asset compiler.

--- scripts/_GameMaker.js
+++ scripts/_GameMaker.js
@@ -697,7 +697,7 @@ function GameMaker_Init()
 	g_Collision_Compatibility_Mode = g_pGMFile.Options.CollisionCompatibility;
     g_Legacy_Primitive_Drawing = g_pGMFile.Options.LegacyPrimitiveDrawing;
 
-    if (g_Legacy_Primitive_Drawing != true)
+    if (g_Legacy_Primitive_Drawing == false)
     {
         offsethackD3D = 0.0;
     }
