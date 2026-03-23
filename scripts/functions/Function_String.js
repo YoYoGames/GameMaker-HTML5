2b4d7cf2e4cba4b1b8790807f94f1e5e73adb119
Fritz <1914610+jackerley@users.noreply.github.com>
2026-03-23 11:57:32 +0000

    [Strings] Fixes for utf-16 to utf-8 conversion
    plus fix for buffer_poke to convert to utf-8 before writing to buffer (as per buffer_write)
    
    Fixes https://github.com/YoYoGames/GameMaker-Bugs/issues/14397 and https://github.com/YoYoGames/GameMaker-Bugs/issues/14395

--- scripts/functions/Function_String.js
+++ scripts/functions/Function_String.js
@@ -659,7 +659,7 @@ function UTF16_to_UTF8(_str)
 {
     var utf8 = [];
     for (var i = 0; i < _str.length; i++) {
-        var charCode = _str.charCodeAt(i);
+        var charCode = _str.codePointAt(i);
         if (charCode < 0x80) {
             utf8.push(charCode);
         }
@@ -667,19 +667,17 @@ function UTF16_to_UTF8(_str)
             utf8.push(0xc0 | (charCode >> 6),
                       0x80 | (charCode & 0x3f));
         }
-        else if (charCode < 0xd800 || charCode >= 0xe000) {
+        else if (charCode < 0x10000) {
             utf8.push(0xe0 | (charCode >> 12),
                       0x80 | ((charCode >> 6) & 0x3f),
                       0x80 | (charCode & 0x3f));
         }
-            // surrogate pair
         else {
-            i++;
-            charCode = ( ( charCode & 0x3ff ) << 10 ) | ( _str.charCodeAt( i ) & 0x3ff );
             utf8.push(0xf0 | (charCode >> 18),
                       0x80 | ((charCode >> 12) & 0x3f),
                       0x80 | ((charCode >> 6) & 0x3f),
                       0x80 | (charCode & 0x3f));
+            i++;  // Skip the low surrogate in the next iteration
         }
     }
     return utf8;
